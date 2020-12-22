e114e4e04aaab7726094627fab66d83530ff6215
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-12-22 15:44:17 +0000

    Update Genbank_annotation_cleanup.py

--- Genbank_annotation_cleanup.py
+++ Genbank_annotation_cleanup.py
@@ -1,8 +1,6 @@
 import csv
 import re 
 
-exon_pattern = "
-
 with open('Anomodom_Annotations.csv', newline='') as csv_file:
 
 # input: a csv with four columns: names, start, end and type
@@ -15,11 +13,10 @@ with open('Anomodom_Annotations.csv', newline='') as csv_file:
     
     
     for row in csv_reader: # each row is a list
-        new_row = [] # create new row for each row for appending into final_list
+        
         name = (row.pop(0)).split() # remove the gene name, name[0] = gene name
         name_list.append(name[0])
        
-        
         if "gene" in name:
             final_list.append(row)
             final_list.append(["", "", "", "gene", name[0]])
@@ -37,34 +34,20 @@ with open('Anomodom_Annotations.csv', newline='') as csv_file:
             final_list.append(row)
             final_list.append(["", "", "", "rRNA", name[0]])
 
-        elif "exon" in name: # can append, cannot add final line 
-            if name[2] == "1":
-                row[2] = "CDS"
+    for row in enumerate(csv_reader):
+            
+        if "exon" in row:
+            row [2] = "CDS"
+            final_list.append(row)
+           
+            if name[0] == next(name[0]):
+                row [2] = ""
                 final_list.append(row)
                 
-            elif name[0] in name_list[-1] and name[1] == "exon":
-                row[2] = ""
-                final_list.append(row)
-
-        else: # we don't need introns 
-            pass
-
-    # for row in final_list: # check row above and row below for adding extra row
-
-        # index_row = final_list.index(row) # translatable is not unique
-
-        # for index_row in range(0, (len(final_list)-3)):
-
-            # if final_list[index_row-1] [2] == "" and final_list[index_row+1] [2] == "gene":
-                # final_list.insert(index_row, ["", "","","product",name_list[index_row]])
-                # final_list.insert(index_row+1, ["", "", "", "transl_table", "11"])
-            
-            # elif final_list[index_row-1] [3] == "product":
-                # final_list.insert(index_row+1, ["", "", "", "transl_table", "11"])
-
-            # elif index_row >= len(final_list):
-                # pass
-
+            else:
+                final_list.append(["","","","product",name[0]])
+                final_list.append(["", "", "", "transl_table", "11"])
+                
 # output: a CSV with species name as header, and everything else in the five column format
 
 with open('Genbank_5Column_Anomodon.csv', 'w+', newline='') as csvfile:
